98519902402e65802c7a7782a234ee007e5379bc
pietro.marilei <pietromrl0@gmail.com>
2024-05-23 10:28:51 +0200

    add forma date to europe time zone

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,192 +1,198 @@
 <script>
-    // @ts-nocheck
-    // import { onMount } from 'svelte';
-    import axios from 'axios'
+	// @ts-nocheck
+	// import { onMount } from 'svelte';
+	import axios from 'axios';
 
-    let name = ''
-    let started = false
+	let name = '';
+	let started = false;
 
-    let hours = 0
-    let minutes = 0
-    let seconds = 0
+	let hours = 0;
+	let minutes = 0;
+	let seconds = 0;
 
-    let count = 0
+	let count = 0;
 
-    let startDate = null
+	let startDate = null;
 
-    /**
+	/**
 	 * @type {number | null | undefined}
 	 */
-    let interval = null
+	let interval = null;
 
-    // onMount(async () => {
+	// onMount(async () => {
 	// 	const records = await fetch(`/tutorial/api/album`);
 	// 	photos = await res.json();
 	// });
 
-    let records = []
+	let records = [];
 
-    const getRecords = () => {
-        axios.get(`/api/getcounterbyuser`, {
-            params: {
-                name
-            }
-        })
-        .then(response => {
-            records = [...response.data.records]
-        })
+	function formatDate(date) {
+		return new Intl.DateTimeFormat('it-IT', {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			hour: '2-digit',
+			minute: '2-digit',
+			second: '2-digit',
+			timeZone: 'Europe/Rome'
+		}).format(new Date(date));
+	}
 
-        console.log(`Got records for user ${name}: `, records)
-    }
+	const getRecords = () => {
+		axios
+			.get(`/api/getcounterbyuser`, {
+				params: {
+					name
+				}
+			})
+			.then((response) => {
+				records = [...response.data.records];
+			});
 
-    const toggleCounter = async () => {
-        if(name === '') {
-            alert("Missing name");
+		console.log(`Got records for user ${name}: `, records);
+	};
 
-            return;
-        }
+	const toggleCounter = async () => {
+		if (name === '') {
+			alert('Missing name');
 
-        started = !started
+			return;
+		}
 
-        if(started === true) {
-            startDate = new Date()
-            seconds = 0
-            minutes = 0
-            hours = 0
+		started = !started;
 
-            console.log('Starting counter for: ', name, startDate)
+		if (started === true) {
+			startDate = new Date();
+			seconds = 0;
+			minutes = 0;
+			hours = 0;
 
-            interval = setInterval(() => {
-                seconds = count % 60
-                minutes = Math.floor(count / 60)
-                hours = Math.floor(count / (60 * 60))
+			console.log('Starting counter for: ', name, startDate);
 
-                if(minutes >= 60) {
-                    minutes %= 60
-                }
+			interval = setInterval(() => {
+				seconds = count % 60;
+				minutes = Math.floor(count / 60);
+				hours = Math.floor(count / (60 * 60));
 
-                if(hours >= 24) {
-                    hours %= 24
-                }
+				if (minutes >= 60) {
+					minutes %= 60;
+				}
 
-                count++
+				if (hours >= 24) {
+					hours %= 24;
+				}
 
-                //console.log(hours, minutes, seconds, count)
-            }, 1000)
-        } else {
-            // @ts-ignore
-            clearInterval(interval)
+				count++;
 
-            axios.post('/api/savecounter', {
-                name,
-                startDate,
-                endDate: new Date(),
-                hours,
-                minutes,
-                seconds,
-            })
-            .then(response => {
-                // Trattamento del risultato positivo
-                console.log('Response:', response.data);
-            })
-            .catch(error => {
-                // Trattamento di errori
-                console.error('Error saving counter:', error);
-            });
-        }
+				//console.log(hours, minutes, seconds, count)
+			}, 1000);
+		} else {
+			// @ts-ignore
+			clearInterval(interval);
 
-        getRecords()
-    }
+			axios
+				.post('/api/savecounter', {
+					name,
+					startDate,
+					endDate: new Date(),
+					hours,
+					minutes,
+					seconds
+				})
+				.then((response) => {
+					// Trattamento del risultato positivo
+					console.log('Response:', response.data);
+				})
+				.catch((error) => {
+					// Trattamento di errori
+					console.error('Error saving counter:', error);
+				});
+		}
+
+		getRecords();
+	};
 </script>
 
 <div id="main" class="container">
-    <div class="row mb-3">
-        <h1>Employee / Collaborator Work Counter</h1>
-    </div>
+	<div class="row mb-3">
+		<h1>Employee / Collaborator Work Counter</h1>
+	</div>
 
-    <div class="row mb-3 mt-3">
-        <div class="col-6 text-center">
-            <h2>
-                {#if !started}
-                    Enter Name
-                {:else}
-                    Welcome {name}
-                {/if}
-            </h2>
-        </div>
-        <div class="col-6">
-            <input type="text" name="name" id="name" bind:value={name}>
-        </div>
-    </div>
+	<div class="row mb-3 mt-3">
+		<div class="col-6 text-center">
+			<h2>
+				{#if !started}
+					Enter Name
+				{:else}
+					Welcome {name}
+				{/if}
+			</h2>
+		</div>
+		<div class="col-6">
+			<input type="text" name="name" id="name" bind:value={name} />
+		</div>
+	</div>
 
-    <div class="row mb-4">
+	<div class="row mb-4">
+		<button on:click={toggleCounter}>
+			{#if started === false}
+				Start Work
+			{:else}
+				Stop
+			{/if}
+		</button>
+	</div>
 
-        <button on:click={toggleCounter}>
-            {#if started === false}
-                Start Work
-            {:else}
-                Stop
-            {/if}
-        </button>
+	{#if name !== ''}
+		<div class="row counter">
+			<div class="col-4">
+				{hours}
+			</div>
+			<div class="col-4">
+				{minutes}
+			</div>
+			<div class="col-4">
+				{seconds}
+			</div>
+		</div>
 
-    </div>
+		<div class="row counter">
+			<div class="col-4">H</div>
+			<div class="col-4">M</div>
+			<div class="col-4">S</div>
+		</div>
+	{/if}
 
-    {#if name !==''}
-        <div class="row counter">
-            <div class="col-4">
-                {hours}
-            </div>
-            <div class="col-4">
-                {minutes}
-            </div>
-            <div class="col-4">
-                {seconds}
-            </div>
-        </div>
+	{#if records.length > 0}
+		<div class="row mb-3">
+			<div class="col">Name</div>
+			<div class="col">StartDate</div>
+			<div class="col">EndDate</div>
+			<div class="col">Time</div>
+		</div>
 
-        <div class="row counter">
-            <div class="col-4">
-                H
-            </div>
-            <div class="col-4">
-                M
-            </div>
-            <div class="col-4">
-                S
-            </div>
-        </div>
-    {/if}
-
-    {#if records.length > 0}
-        <div class="row mb-3">
-            <div class="col">Name</div>
-            <div class="col">StartDate</div>
-            <div class="col">EndDate</div>
-            <div class="col">Time</div>
-        </div>
-
-        {#each records as {name, startDate, endDate, hours, minutes, seconds}}
-            <div class="row">
-                <div class="col">{name}</div>
-                <div class="col">{startDate}</div>
-                <div class="col">{endDate}</div>
-                <div class="col">
-                    {hours} : {minutes} : {seconds}
-                </div>
-            </div>
-        {/each}
-    {:else}
-            <b>No records yet for this user</b>
-    {/if}
+		{#each records as { name, startDate, endDate, hours, minutes, seconds }}
+			<div class="row">
+				<div class="col">{name}</div>
+				<div class="col">{formatDate(startDate)}</div>
+				<div class="col">{formatDate(startDate)}</div>
+				<div class="col">
+					{hours} : {minutes} : {seconds}
+				</div>
+			</div>
+		{/each}
+	{:else}
+		<b>No records yet for this user</b>
+	{/if}
 </div>
 
 <style>
-    #main {
-        padding: 4rem;
-    }
+	#main {
+		padding: 4rem;
+	}
 
-    .counter {
-        font-size: 4rem;
-        text-align: center;
-    }
+	.counter {
+		font-size: 4rem;
+		text-align: center;
+	}
 </style>
